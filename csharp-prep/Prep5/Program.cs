d65d4eb1eefbf8da8c4ca0bfcc75f3348c3de127
Mnhthompson <mnhthompson@gmail.com>
2024-04-22 11:20:50 -0700

    part 5 practice functions

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,64 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+
+        string username = PromptUserName();
+        int usernumber = PromptUserNumber();
+
+        int squared = SquareNumber(usernumber);
+
+        DisplayResult(username, squared);
+
+
     }
+
+        
+        static void DisplayWelcome()
+
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+
+        static string PromptUserName()
+
+        {
+            Console.Write("Please input your name: ");
+            string name = Console.ReadLine();
+
+            return name;
+
+        }
+
+
+        static int PromptUserNumber()
+
+        {
+        Console.Write("Please input your desired number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        return number;
+        }
+
+
+        static int SquareNumber(int number)
+
+        {
+              int square = (number) * (number);
+              return square;
+
+        }
+
+
+
+        static void DisplayResult(string name, int square)
+
+        {
+            Console.WriteLine($"{name}, the square of your number is {square}");
+        }
+
+
+
+
+    
 }
