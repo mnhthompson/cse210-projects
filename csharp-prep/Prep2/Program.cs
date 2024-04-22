868a91439065a6469e282dce7e317335f9882a32
Mnhthompson <mnhthompson@gmail.com>
2024-04-22 10:13:57 -0700

    grade assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What percentage did you get?");
+        string percent = Console.ReadLine();
+
+        int x = int.Parse(percent);
+
+        string alpha = "";
+
+
+        if (x >= 90)
+        {
+             alpha = ("A");
+        }
+          
+
+        else if (x >= 80)
+            {
+             alpha = ("B");}
+
+        else if (x >= 70)
+            {
+             alpha = ("C");}
+
+        else if (x >= 60)
+            {
+             alpha = ("D");}
+        else 
+            {
+             alpha = ("F");}
+
+        Console.WriteLine($"Your Letter grade is: {alpha}");
+
+        if (x >= 70)
+            {Console.WriteLine("You pass");}
+
+
+        else 
+           {Console.WriteLine("You fail"); }
     }
 }
