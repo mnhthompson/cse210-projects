2702d0f347eb0487363c84b18e4196683a54cb00
Mnhthompson <mnhthompson@gmail.com>
2024-04-22 10:14:26 -0700

    change 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -6,6 +6,7 @@ class Program
     {
         Console.WriteLine("What is your first name?");
         string first = Console.ReadLine();
+        
         Console.WriteLine("What is your last name?");
         string last = Console.ReadLine();
 
