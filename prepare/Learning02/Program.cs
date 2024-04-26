dfce9864113e600f93a8de6abdab59b0f04ee944
Mnhthompson <mnhthompson@gmail.com>
2024-04-26 07:53:24 -0700

    prepare 1

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+
+        job1._jobTitle = "Astronaut";
+        job1._company = "Nasa";
+        job1._startYear = 1999;
+        job1._endYear = 2019;
+
+        Job job2 = new Job();
+
+        job2._jobTitle = "Villian";
+        job2._company = "Detective Comics";
+        job2._startYear = 1939;
+        job2._endYear = 2024;
+
+        Resume myResume = new Resume();
+        
+        myResume._name = "Joe Chill";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
